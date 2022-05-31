13d9595e8e9ca62f92314653c73a01d360264c19
Brian Bodiya <bbodiya@akamai.com>
2022-05-31 13:18:47 -0400

    update curl and remove unused variables

--- queue-event.sh
+++ queue-event.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -eo pipefail
-curl --insecure --cert $TESTNET_CERT --key $TESTNET_KEY -X POST "https://sifter.default.abattery.appbattery.nss1.tn.akamai.com:4433/api/v2/queue/git" -H  "accept: application/json" -H  "X-DEBUG-FLAG: false" -H  "Content-Type: application/json" -d "{  \"project\": \"~bbodiya\",  \"repo\": \"gitlab-connectivity-test\",  \"branch\": \"main\"}"
+/usr/bin/curl --insecure --cert $TESTNET_CERT --key $TESTNET_KEY -X POST "https://sifter.default.abattery.appbattery.nss1.tn.akamai.com:4433/api/v2/queue/git" -H  "accept: application/json" -H  "X-DEBUG-FLAG: false" -H  "Content-Type: application/json" -d "{  \"project\": \"~bbodiya\",  \"repo\": \"gitlab-connectivity-test\",  \"branch\": \"main\"}"
