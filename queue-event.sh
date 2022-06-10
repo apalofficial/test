4c88ee1596e0cffcb0ded99caa6ee4e38901f1c0
Brian Bodiya <bbodiya@akamai.com>
2022-06-10 14:13:37 -0400

    fix path, and make curl silent

--- queue-event.sh
+++ queue-event.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -eo pipefail
-/usr/bin/curl --insecure --cert $TESTNET_CERT --key $TESTNET_KEY -X POST "https://sifter.default.hh.appbattery.shared.qa.akamai.com:4333/api/v2/queue/git" -H  "accept: application/json" -H  "X-DEBUG-FLAG: false" -H  "Content-Type: application/json" -d "{  \"project\": \"UN\",  \"repo\": \"gitlab-connectivity-test\",  \"branch\": \"main\"}"
+/usr/bin/curl -s --insecure --cert $TESTNET_CERT --key $TESTNET_KEY -X POST "https://sifter.default.hh.appbattery.shared.qa.akamai.com:4333/api/v2/queue/git" -H  "accept: application/json" -H  "X-DEBUG-FLAG: false" -H  "Content-Type: application/json" -d "{  \"project\": \"UN\",  \"repo\": \"gitlab-connectivity-test\",  \"branch\": \"main\"}"
