1cb3db26c100856d23c3e63d7912876da27d9786
Brian Bodiya <bbodiya@akamai.com>
2022-06-12 22:25:40 -0400

    fix image name

--- job_scripts/deploy.sh
+++ job_scripts/deploy.sh
@@ -7,12 +7,12 @@ export JOB_ID=9d9be53a-eaaf-11ec-ade4-0242c0a8080e
 mkdir /tmp/registry-certs
 cp $TESTNET_KEY /tmp/registry-certs/user.key
 cp $TESTNET_CERT /tmp/registry-certs/user.cert
-skopeo copy --src-tls-verify=false --src-cert-dir /tmp/registry-certs docker://staging.docker.akamai.com:5433/build-images-development/basic_connector/$STAGE:0.1-$JOB_ID containers-storage:staging.docker.akamai.com/build-images-development/basic_connector/$STAGE:0.1-$JOB_ID 
+skopeo copy --src-tls-verify=false --src-cert-dir /tmp/registry-certs docker://staging.docker.akamai.com:5433/build-images-development/basic_connector/$IMAGE:0.1-$JOB_ID containers-storage:staging.docker.akamai.com/build-images-development/basic_connector/$IMAGE:0.1-$JOB_ID 
 rm -rf /tmp/registry-certs
 podman system connection add --identity $CI_DEPLOY_KEY hello-ci ssh://$PODMAN_USER@hello-ci.akamai.lol:22222/run/user/$PODMAN_UID/podman/podman.sock
-podman image scp staging.docker.akamai.com/build-images-development/basic_connector/$STAGE:0.1-$JOB_ID hello-ci::
+podman image scp staging.docker.akamai.com/build-images-development/basic_connector/$IMAGE:0.1-$JOB_ID hello-ci::
 set +e
 podman --remote stop ci
 set -e
-podman --remote run --rm --name ci -d --network=host staging.docker.akamai.com/build-images-development/basic_connector/$STAGE:0.1-$JOB_ID
+podman --remote run --rm --name ci -d --network=host staging.docker.akamai.com/build-images-development/basic_connector/$IMAGE:0.1-$JOB_ID
 podman --remote image prune -fa
