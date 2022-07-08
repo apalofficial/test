e10abacc8a404f9181be47f4966a841ae05d3063
Brian Bodiya <bbodiya@akamai.com>
2022-07-08 10:43:03 -0400

    change sed delimiters

--- job_scripts/k8s_deploy.sh
+++ job_scripts/k8s_deploy.sh
@@ -11,6 +11,6 @@ KUBECONFIG_DIR=$(dirname $K8S_KUBECONFIG)
 
 podman run --rm --env KUBECONFIG=$K8S_KUBECONFIG -v $KUBECONFIG_DIR:$KUBECONFIG_DIR -v $PWD:/workspace --workdir /workspace docker.io/rancher/kubectl:v1.23.7 get deployment k8s-qa -o yaml --namespace gitlab > artifacts/qa-deployment.yaml
 
-sed -i -- "s/\(- image: \).*/\1$REGISTRY_IMAGE/" artifacts/qa-deployment.yaml
+sed -i -- "s;\(- image: \).*;\1$REGISTRY_IMAGE;" artifacts/qa-deployment.yaml
 
 podman run --rm --env KUBECONFIG=$K8S_KUBECONFIG -v $KUBECONFIG_DIR:$KUBECONFIG_DIR -v $PWD:/workspace --workdir /workspace docker.io/rancher/kubectl:v1.23.7 apply -f artifacts/qa-deployment.yaml
