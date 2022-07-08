bb27583e4be11a761261e0c009f54254894f2f2e
Brian Bodiya <bbodiya@akamai.com>
2022-07-08 15:51:04 -0400

    add prod k8s deploy

--- job_scripts/k8s_deploy.sh
+++ job_scripts/k8s_deploy.sh
@@ -2,15 +2,17 @@ set -exo pipefail
 echo "Deploying application..."
 
 podman pull $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA
-REGISTRY_IMAGE="registry.linode.lol/basic_connector/image:0.1-$CI_JOB_ID"
+REGISTRY_IMAGE="registry.linode.lol/basic_connector/image:0.1-$JOB_ID"
 
 podman tag $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA $REGISTRY_IMAGE
 podman push $REGISTRY_IMAGE
 mkdir artifacts
 KUBECONFIG_DIR=$(dirname $K8S_KUBECONFIG)
 
-podman run --rm --env KUBECONFIG=$K8S_KUBECONFIG -v $KUBECONFIG_DIR:$KUBECONFIG_DIR -v $PWD:/workspace --workdir /workspace docker.io/rancher/kubectl:v1.23.7 get deployment k8s-qa -o yaml --namespace gitlab > artifacts/qa-deployment.yaml
+YAML_FILE="artifacts/$K8S_ENV-deployment.yaml"
 
-sed -i -- "s;\(- image: \).*;\1$REGISTRY_IMAGE;" artifacts/qa-deployment.yaml
+podman run --rm --env KUBECONFIG=$K8S_KUBECONFIG -v $KUBECONFIG_DIR:$KUBECONFIG_DIR -v $PWD:/workspace --workdir /workspace docker.io/rancher/kubectl:v1.23.7 get deployment "k8s-$K8S_ENV" -o yaml --namespace gitlab > $YAML_FILE
 
-podman run --rm --env KUBECONFIG=$K8S_KUBECONFIG -v $KUBECONFIG_DIR:$KUBECONFIG_DIR -v $PWD:/workspace --workdir /workspace docker.io/rancher/kubectl:v1.23.7 apply -f artifacts/qa-deployment.yaml
+sed -i -- "s;\(- image: \).*;\1$REGISTRY_IMAGE;" $YAML_FILE
+
+podman run --rm --env KUBECONFIG=$K8S_KUBECONFIG -v $KUBECONFIG_DIR:$KUBECONFIG_DIR -v $PWD:/workspace --workdir /workspace docker.io/rancher/kubectl:v1.23.7 apply -f $YAML_FILE
