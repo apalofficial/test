667c64872e32226a7c1e84ab91fbb8e64c31e276
Brian Bodiya <bbodiya@akamai.com>
2022-07-08 20:54:50 -0400

    remove use of skopeo

--- job_scripts/pull-from-artifactory.sh
+++ job_scripts/pull-from-artifactory.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -exo pipefail
 echo "Deploying application..."
-dnf -y install skopeo #required for image pull at non-standard port
 mkdir /tmp/registry-certs
 cp $TESTNET_KEY /tmp/registry-certs/user.key
 cp $TESTNET_CERT /tmp/registry-certs/user.cert
-skopeo copy --src-tls-verify=false --src-cert-dir /tmp/registry-certs docker://docker.akamai.com/build-images-development/basic_connector/image:0.1-$JOB_ID containers-storage:docker.akamai.com/build-images-development/basic_connector/image:0.1-$JOB_ID 
+podman pull --tls-verify=false --cert-dir /tmp/registry-certs docker.akamai.com/build-images-development/basic_connector/image:0.1-$JOB_ID
 rm -rf /tmp/registry-certs
