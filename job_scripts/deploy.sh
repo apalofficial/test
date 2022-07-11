990680edc382daffbc9c1359a874de3f212244a3
Brian Bodiya <bbodiya@akamai.com>
2022-07-11 10:40:11 -0400

    refactor

--- job_scripts/deploy.sh
+++ job_scripts/deploy.sh
@@ -2,9 +2,9 @@
 set -exo pipefail
 echo "Deploying application..."
 podman system connection add --identity $CI_DEPLOY_KEY hello-ci ssh://$PODMAN_USER@hello-ci.akamai.lol:22222/run/user/$PODMAN_UID/podman/podman.sock
-podman image scp docker.akamai.com/build-images-development/basic_connector/image:0.1-$JOB_ID hello-ci::
+podman image scp $SOURCE_IMAGE hello-ci::
 set +e
 podman --remote stop ci
 set -e
-podman --remote run --rm --name ci -d --network=host docker.akamai.com/build-images-development/basic_connector/image:0.1-$JOB_ID
+podman --remote run --rm --name ci -d --network=host $SOURCE_IMAGE
 podman --remote image prune -fa
--- job_scripts/pull-from-artifactory.sh
+++ job_scripts/pull-from-artifactory.sh
@@ -4,5 +4,5 @@ echo "Pulling image..."
 mkdir /tmp/registry-certs
 cp $TESTNET_KEY /tmp/registry-certs/user.key
 cp $TESTNET_CERT /tmp/registry-certs/user.cert
-podman pull --tls-verify=false --cert-dir /tmp/registry-certs docker.akamai.com/build-images-development/basic_connector/image:0.1-$JOB_ID
+podman pull --tls-verify=false --cert-dir /tmp/registry-certs $SOURCE_IMAGE
 rm -rf /tmp/registry-certs
